531b25a531829bb56d98552f9078ae4d05397a34
Sunset <sunrisebestrise@gmail.com>
2019-07-09 14:47:55 +0200

    trying to make link move

--- Tilemap/main.lua
+++ Tilemap/main.lua
@@ -23,7 +23,7 @@ function love.load()
 	-- Create a Custom Layer
 	map:addCustomLayer("Sprite Layer", 3)
 
-	-- Add data to Custom Layer
+	--[[ Add data to Custom Layer
 	local spriteLayer = map.layers["Sprite Layer"]
 	spriteLayer.sprites = {
 		player = {
@@ -31,10 +31,16 @@ function love.load()
 			x = windowWidth / 2,
 			y = windowHeight / 2,
 		}
+	}]]--
+
+	player = {
+		image = love.graphics.newImage("assets/link.png"),
+		x = windowWidth / 2,
+		y = windowHeight / 2,
 	}
 
 	-- Update callback for Custom Layer
-	function spriteLayer:update(dt)
+	--[[function spriteLayer:update(dt)
 		
 	end
 
@@ -46,6 +52,8 @@ function love.load()
 			love.graphics.draw(sprite.image, x, y)
 		end
 	end
+end]]--
+
 end
 
 function love.update(dt)
@@ -54,24 +62,26 @@ function love.update(dt)
 	-- Move map
 	local kd = love.keyboard.isDown
 	tx = (kd("a", "left")  and tx - 128 * dt) or tx -- if button pressed then change tx, otherwise don't change tx
-	tx = (kd("d", "right") and tx + 128 * dt) or tx
-	ty = (kd("w", "up")    and ty - 128 * dt) or ty
-	ty = (kd("s", "down")  and ty + 128 * dt) or ty
-end
+		tx = (kd("d", "right") and tx + 128 * dt) or tx
+		ty = (kd("w", "up")    and ty - 128 * dt) or ty
+		ty = (kd("s", "down")  and ty + 128 * dt) or ty
+	end
 
-function love.draw()
-	-- Draw the map and all objects within
-	love.graphics.setColor(1, 1, 1)
-	map:draw(-tx, -ty)
+	function love.draw()
+		-- Draw the map and all objects within
+		love.graphics.setColor(1, 1, 1)
+		map:draw(-tx, -ty)
 
-	-- Draw Collision Map (useful for debugging)
-	love.graphics.setColor(1, 0, 0)
-	map:box2d_draw()
+		-- Draw Collision Map (useful for debugging)
+		love.graphics.setColor(1, 0, 0)
+		map:box2d_draw()
 
-	-- Translate world so that player is always centred
+		love.graphics.draw(player.image, player.x, player.y)
+
+	--[[ Translate world so that player is always centred
 	local player = map.layers["Sprite Layer"].sprites.player
 	local tx = math.floor(player.x - love.graphics.getWidth()  / 2)
-	local ty = math.floor(player.y - love.graphics.getHeight() / 2)
+	local ty = math.floor(player.y - love.graphics.getHeight() / 2)]]--
 
 	-- Please note that map:draw, map:box2d_draw, and map:bump_draw take
 	-- translate and scale arguments (tx, ty, sx, sy) for when you want to
